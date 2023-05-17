06d3409c493094067bcbd2189eb012a962f58e92
chaeyuun <chaeyuun32@gmail.com>
2023-05-17 14:18:16 +0900

    Updates

---
+++ precache-manifest.cbb9d703d00adb27a348b3af7655ec47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b96665a010761d5bafde",
+    "url": "/test/static/css/main.b61fbf94.chunk.css"
+  },
+  {
+    "revision": "b96665a010761d5bafde",
+    "url": "/test/static/js/main.b96665a0.chunk.js"
+  },
+  {
+    "revision": "2aa1e1831c175d60f876",
+    "url": "/test/static/js/1.2aa1e183.chunk.js"
+  },
+  {
+    "revision": "5ad1e219c88e053e0b1e",
+    "url": "/test/static/js/runtime~main.5ad1e219.js"
+  },
+  {
+    "revision": "56db0f7e31469b247175251cbc036781",
+    "url": "/test/index.html"
+  }
+];
